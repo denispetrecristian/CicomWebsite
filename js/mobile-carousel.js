59a66ed86f7f47a0657d0f27d60f2558e65bbb5c
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2020-12-22 15:04:42 +0200

    Add handle on resize of the partners page

--- js/mobile-carousel.js
+++ js/mobile-carousel.js
@@ -33,6 +33,7 @@ function showSlides(index) {
 
 }
 
+//Use the medium breakpoint
 function resizeHandle() {
     if (screen.width > 920) {
         showOnBigScreen();
@@ -42,11 +43,19 @@ function resizeHandle() {
 }
 
 function showOnSmallScreen() {
+    var section_small = document.getElementById("hidden-small-carousel");
+    var section_big = document.getElementById("hidden-big-carousel");
 
+    section_small.style.display = "block";
+    section_big.style.display = "none";
 }
 
 function showOnBigScreen() {
+    var section_big = document.getElementById("hidden-big-carousel");
+    var section_small = document.getElementById("hidden-small-carousel");
 
+    section_big.style.display = "block";
+    section_small.style.display = "none";
 }
 
 window.addEventListener('resize', resizeHandle);
