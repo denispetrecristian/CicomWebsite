7ff2eb9bc8466ab24a1919ea077023b36765a32b
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2020-12-22 14:29:19 +0200

    Add js control for the carousel on the partners page

---
+++ js/mobile-carousel.js
@@ -0,0 +1,42 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(index) {
+    showSlides(slideIndex += index);
+};
+
+function currentSlide(index) {
+    showSlides(slideIndex = index);
+}
+
+function showSlides(index) {
+    var i;
+
+    //Get the slides
+    var slides = document.getElementsByClassName("slide-logo");
+
+    //Check for wraping around
+    if (index > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (index < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+
+
+}
+
+function showOnSmallScreen() {
+
+}
+
+function showOnBigScreen() {
+
+}
