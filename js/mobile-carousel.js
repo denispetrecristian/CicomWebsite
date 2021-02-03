e831b3a827d792aa8a4a3909e0a6f7342322599d
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2021-02-03 15:44:59 +0200

    Add script to hide elements on the partners page

--- js/mobile-carousel.js
+++ js/mobile-carousel.js
@@ -33,33 +33,4 @@ function showSlides(index) {
 
 }
 
-//Use the medium breakpoint
-function resizeHandle() {
-    console.log(window.innerWidth);
-    if (window.innerWidth < 920) {
-        showOnBigScreen();
 
-    } else {
-        showOnSmallScreen();
-
-    }
-}
-
-function showOnSmallScreen() {
-    var section_small = document.getElementById("hidden-small-carousel");
-    var section_big = document.getElementById("hidden-big-carousel");
-
-    section_small.style.display = "block";
-    section_big.style.display = "none";
-}
-
-function showOnBigScreen() {
-    var section_big = document.getElementById("hidden-big-carousel");
-    var section_small = document.getElementById("hidden-small-carousel");
-
-    section_big.style.display = "block";
-    section_small.style.display = "none";
-}
-
-window.addEventListener('resize', resizeHandle);
-window.addEventListener('DOMContentLoaded', resizeHandle);
